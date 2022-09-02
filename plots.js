8c878493694d68f883595a639d2e89c5260f322a
ranedigamber <rane.24@osu.edu>
2022-09-01 22:42:35 -0400

    Update 09/01, 10:42

--- plots.js
+++ plots.js
@@ -134,36 +134,34 @@ function buildCharts(sample) {
       
       // 4. Create the trace for the gauge chart.
     
-    var gaugeData = [
-        {
-            domain: { x: [0, 1], y: [0, 1] },
+      var gaugeData = [{
+        domain: { x: [0, 1], y: [0, 1] },
+        value: washing_frequency,
+        type: "indicator",
+        mode: "gauge+number",
+        title: { text: "<b> Belly Button Washing Frequency</b> <br> # of Scrubs per Week" },
+        gauge: {
+          axis: { range: [null, 10], tickwidth: 2, tickcolor: "black" },
+          bar: { color: "black" },
+          steps: [
+            { range: [0, 2], color: "firebrick" },
+            { range: [2, 4], color: "darkorange" },
+            { range: [4, 6], color: "greenyellow" },
+            { range: [6, 8], color: "lightseagreen" },
+            { range: [8, 10], color: "dodgerblue" }
+          ],
+          threshold: {
             value: washing_frequency,
-            type: "indicator",
-            mode: "gauge+number",
-            title: {text: "<b> Belly Button Washing Frequency</b> <br> # of Scrubs per Week" },
-            guage: {
-                axis: {range: [null, 10], tickwidth: 2, tickcolor: "black" },
-                bar: { color: "black" },
-                steps: [
-                    {range: [0, 2], color: "firebrick"},
-                    {range: [2, 4], color: "darkorange"},
-                    {range: [4, 6], color: "greenyellow"},
-                    {range: [6, 8], color: "lightseagreen"},
-                    {range: [8, 10], color: "dodgerblue"}
-                ],
-                threshold: {
-                    value: washing_frequency,
-                }
-            },
-        }];
-    
-    // 5. Create the layout for the gauge chart.
-    var gaugeLayout = { 
-     width: 600, height: 500, margin: { t: 0, b: 0 },
-     font: {color: "black"}
-    };
-
-    // 6. Use Plotly to plot the gauge data and layout.
-    Plotly.newPlot("gauge", gaugeData, gaugeLayout);
-  });
-}
+          }
+        },
+        
+      }];
+      // 5. Create the layout for the gauge chart.
+      var gaugeLayout = { 
+        width: 600, height: 500, margin: { t: 0, b: 0 },
+        font: { color: "black"}
+      };
+      // 6. Use Plotly to plot the gauge data and layout.
+      Plotly.newPlot("gauge", gaugeData, gaugeLayout);
+    });
+  }
